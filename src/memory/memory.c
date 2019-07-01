75d1118835c72e149d9af7951617c4a90aee27a0
Justinien Bouron <justinien.bouron@gmail.com>
2019-07-01 01:36:40 -0700

    Add memzero function.

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,15 +1,20 @@
 #include <memory/memory.h>
 
 void
-memcpy(char *const to, char const *const from, size_t const len) {
+memcpy(uint8_t *const to, uint8_t const *const from, size_t const len) {
     for (size_t i = 0; i < len; ++i) {
         to[i] = from[i];
     }
 }
 
 void
-memset(char *const to, char const byte, size_t const len) {
+memset(uint8_t *const to, uint8_t const byte, size_t const len) {
     for (size_t i = 0; i < len; ++i) {
         to[i] = byte;
     }
+}
+
+void
+memzero(uint8_t *const to, size_t const len) {
+    memset(to,0,len);
 }
--- src/memory/memory.h
+++ src/memory/memory.h
@@ -5,9 +5,13 @@
 // Copy a memory region to another.
 // Copy `len` bytes.
 void
-memcpy(char *const to, char const *const from, size_t const len);
+memcpy(uint8_t *const to, uint8_t const *const from, size_t const len);
 
 // Fill a memory region with `len` bytes to the value `byte`.
 void
-memset(char *const to, char const byte, size_t const len);
+memset(uint8_t *const to, uint8_t const byte, size_t const len);
+
+// Fill a memory region with `len` bytes to the value 0.
+void
+memzero(uint8_t *const to, size_t const len);
 #endif
