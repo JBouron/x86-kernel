564cff86e88a99aaf340ababe4e01790275542ed
Justinien Bouron <justinien.bouron@gmail.com>
2019-12-19 14:26:34 -0800

    Add testing for math functions + rename.

--- src/math.c
+++ src/math.c
@@ -1,13 +1,15 @@
 #include <math.h>
 
-uint32_t max(uint32_t const x, uint32_t const y) {
+uint32_t max_u32(uint32_t const x, uint32_t const y) {
     return ((x) > (y)) ? (x) : (y);
 }
 
-uint32_t min(uint32_t const x, uint32_t const y) {
+uint32_t min_u32(uint32_t const x, uint32_t const y) {
     return ((x) < (y)) ? (x) : (y);
 }
 
-uint32_t ceil_x_over_y(uint32_t const x, uint32_t const y) {
+uint32_t ceil_x_over_y_u32(uint32_t const x, uint32_t const y) {
     return ((x) / (y)) + (((x) % (y)) != 0);
 }
+
+#include <math.test>
--- src/math.h
+++ src/math.h
@@ -2,10 +2,12 @@
 
 #include <types.h>
 
-// Computes ceil(x/y).
-uint32_t ceil_x_over_y(uint32_t const x, uint32_t const y);
+// Compute ceil(x/y).
+uint32_t ceil_x_over_y_u32(uint32_t const x, uint32_t const y);
 
 // Compute min/max(x, y).
-uint32_t max(uint32_t const x, uint32_t const y);
-uint32_t min(uint32_t const x, uint32_t const y);
+uint32_t max_u32(uint32_t const x, uint32_t const y);
+uint32_t min_u32(uint32_t const x, uint32_t const y);
 
+// Test the functions defined above.
+void math_test(void);
