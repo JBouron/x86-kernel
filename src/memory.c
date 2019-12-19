b5cf492373a2200c2433b3965d55d762966b2d28
Justinien Bouron <justinien.bouron@gmail.com>
2019-12-19 14:26:54 -0800

    Add testing for memory ops.

--- src/memory.c
+++ src/memory.c
@@ -1,7 +1,6 @@
 #include <memory.h>
 
-void
-memcpy(void * const to, void const * const from, size_t const len) {
+void memcpy(void * const to, void const * const from, size_t const len) {
     uint8_t * const __to = (uint8_t*)to;
     uint8_t const * const __from = (uint8_t*)from;
     for (size_t i = 0; i < len; ++i) {
@@ -9,15 +8,16 @@ memcpy(void * const to, void const * const from, size_t const len) {
     }
 }
 
-void
-memset(void * const to, uint8_t const byte, size_t const len) {
+void memset(void * const to, uint8_t const byte, size_t const len) {
     uint8_t * const __to = (uint8_t*)to;
     for (size_t i = 0; i < len; ++i) {
         __to[i] = byte;
     }
 }
 
-void
-memzero(void * const to, size_t const len) {
+void memzero(void * const to, size_t const len) {
     memset(to, 0, len);
 }
+
+// Testing.
+#include <memory.test>
--- src/memory.h
+++ src/memory.h
@@ -1,15 +1,25 @@
 // Memory manipulation functions.
 #pragma once
 #include <types.h>
-// Copy a memory region to another.
-// Copy `len` bytes.
-void
-memcpy(void * const to, void const * const from, size_t const len);
 
-// Fill a memory region with `len` bytes to the value `byte`.
-void
-memset(void * const to, uint8_t const byte, size_t const len);
+// Copy a memory region to another _in the same memory address space_.
+// @param to: The destination of the copy.
+// @param fomr: The source of the copy.
+// @param len: The number of bytes to copy from `from` to `to`.
+// Note: The copy is performed on a byte basis.
+void memcpy(void * const to, void const * const from, size_t const len);
 
-// Fill a memory region with `len` bytes to the value 0.
-void
-memzero(void * const to, size_t const len);
+// Fill a memory region with a certain value.
+// @param to: The memory region to fill.
+// @param byte: The value to write in each byte of the memory region.
+// @param len: The number of bytes in the memory region.
+void memset(void * const to, uint8_t const byte, size_t const len);
+
+// Fill a memory region with NULL bytes. This is a short-hand for memset(to, 0,
+// len).
+// @param to: The memory region to fill.
+// @param len: The number of bytes in the memory region.
+void memzero(void * const to, size_t const len);
+
+// Execute tests of the above functions.
+void mem_test(void);
