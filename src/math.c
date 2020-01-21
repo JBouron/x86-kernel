ddc73d4de037d618c5d274a9044ebfe5689b5777
Justinien Bouron <justinien.bouron@gmail.com>
2020-01-20 21:05:57 -0800

    Add min/max for uint64_t and round up/down for uint32_t.

--- src/math.c
+++ src/math.c
@@ -8,8 +8,24 @@ uint32_t min_u32(uint32_t const x, uint32_t const y) {
     return ((x) < (y)) ? (x) : (y);
 }
 
+uint64_t max_u64(uint64_t const x, uint64_t const y) {
+    return ((x) > (y)) ? (x) : (y);
+}
+
+uint64_t min_u64(uint64_t const x, uint64_t const y) {
+    return ((x) < (y)) ? (x) : (y);
+}
+
 uint32_t ceil_x_over_y_u32(uint32_t const x, uint32_t const y) {
     return ((x) / (y)) + (((x) % (y)) != 0);
+}
+
+uint32_t round_up_u32(uint32_t const x, uint32_t const m) {
+    return ((x / m) + (x % m != 0)) * m;
+}
+
+uint32_t round_down_u32(uint32_t const x, uint32_t const m) {
+    return (x / m) * m;
 }
 
 #include <math.test>
--- src/math.h
+++ src/math.h
@@ -5,9 +5,14 @@
 // Compute ceil(x/y).
 uint32_t ceil_x_over_y_u32(uint32_t const x, uint32_t const y);
 
+uint32_t round_up_u32(uint32_t const x, uint32_t const m);
+uint32_t round_down_u32(uint32_t const x, uint32_t const m);
+
 // Compute min/max(x, y).
 uint32_t max_u32(uint32_t const x, uint32_t const y);
 uint32_t min_u32(uint32_t const x, uint32_t const y);
+uint64_t max_u64(uint64_t const x, uint64_t const y);
+uint64_t min_u64(uint64_t const x, uint64_t const y);
 
 // Test the functions defined above.
 void math_test(void);
