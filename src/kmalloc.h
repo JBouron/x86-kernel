8034b6d59b362692c0f6e14c3b948eddd18a5622
Justinien Bouron <justinien.bouron@gmail.com>
2020-01-24 22:48:43 -0800

    Add missing interface for kmalloc and kfree.

--- src/kmalloc.h
+++ src/kmalloc.h
@@ -1,3 +1,15 @@
 #pragma once
 
+// Allocate memory.
+// @param size: The requested amount of memory.
+// @return: The virtual address of the allocated memory.
+void * kmalloc(size_t const size);
+
+// Free allocated memory.
+// @param addr: The address of the memory to free. This cannot point in the
+// middle of an allocated buffer. It _must_ point to the very first byte of the
+// allocated buffer.
+void kfree(void * const addr);
+
+// Execute memory allocation tests.
 void kmalloc_test(void);
