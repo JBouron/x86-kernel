a0d201a837c4d8a35643ef9b83d68959ffe295e0
Justinien Bouron <justinien.bouron@gmail.com>
2020-01-28 20:01:49 -0800

    Test runner: Add memory leak detector to detect test function allocating physical frames without freeing them.

--- src/test.c
+++ src/test.c
@@ -1,5 +1,6 @@
 #include <test.h>
 #include <debug.h>
+#include <frame_alloc.h>
 
 // Some test statistics:
 // The number of tests run so far.
@@ -9,10 +10,16 @@ static uint32_t SUCCESS_COUNT = 0;
 
 void __run_single_test(test_function const func, char const * const name) {
     TESTS_COUNT ++;
+    uint32_t const allocated_frames_before = frames_allocated();
     bool const res = func();
+    uint32_t const allocated_frames_after = frames_allocated();
     SUCCESS_COUNT += res ? 1 : 0;
     char const * const str = res ? " OK " : "FAIL";
     LOG("[%s] %s\n", str, name);
+    if (allocated_frames_before != allocated_frames_after) {
+        uint32_t const num = allocated_frames_after - allocated_frames_before;
+        LOG("   Physical frame leak of %u frames detected for %s\n", num, name);
+    }
 }
 
 void print_test_summary(void) {
