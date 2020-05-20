e18472e583616e7e825d98155dc950ebee5ef61f
Justinien Bouron <justinien.bouron@gmail.com>
2020-05-20 16:56:50 -0700

    Spinlock: Disable interrupt flag during the critical section. The flag is restored upon releasing a spinlock.

--- src/lock.h
+++ src/lock.h
@@ -1,10 +1,17 @@
 #pragma once
+#include <percpu.h>
+#include <cpu.h>
 
 // Spinlock state.
 struct spinlock_t {
     // The underlying lock. If the lock is free, this field is 0, otherwise it
     // is 1.
     uint8_t lock;
+    // The state of the interrupt flag of the cpu holding this lock _before_
+    // acquiring the lock. This allows us to disable interrupts in critical
+    // sections and restore them after releasing the lock (if they were enabled
+    // before acquiring the lock).
+    bool interrupts_enabled;
 };
 
 #define INIT_SPINLOCK() \
@@ -14,6 +21,18 @@ struct spinlock_t {
 
 #define DECLARE_SPINLOCK(name)  \
     struct spinlock_t name = INIT_SPINLOCK();
+
+// INTERNAL. Do the actual lock operation on the spinlock. This function should
+// never be used outside the implementation of spinlocks. Use spinlock_lock
+// instead.
+// @param lock: The spinlock to acquire.
+void _spinlock_lock(struct spinlock_t * const lock);
+
+// INTERNAL. Do the actual unlock operation on the spinlock. This function
+// should never be used outside the implementation of spinlocks. Use
+// spinlock_unlock instead.
+// @param lock: The spinlock to release.
+void _spinlock_unlock(struct spinlock_t * const lock);
 
 // Acquire the lock on a spinlock. Returns only when the lock has been acquired
 // by the current cpu.
---
+++ src/lock_def.c
@@ -0,0 +1,26 @@
+#include <lock.h>
+#include <cpu.h>
+#include <debug.h>
+
+// This file contains the definitions of spinlock_lock and spinlock_unlock.
+
+void spinlock_lock(struct spinlock_t * const lock) {
+    // We don't have a way to atomically acquire a lock and disable interrupts,
+    // therefore disable interrupts even before trying to acquire the lock.
+    bool const irq = interrupts_enabled();
+    cpu_set_interrupt_flag(false);
+
+    _spinlock_lock(lock);
+
+    // We can now mutate the fields of the lock.
+    lock->interrupts_enabled = irq;
+}
+
+void spinlock_unlock(struct spinlock_t * const lock) {
+    // Get back the saved IF from the lock.
+    bool const interrupts = lock->interrupts_enabled;
+
+    _spinlock_unlock(lock);
+    // Restore the IF to its previous value before the spinlock_lock.
+    cpu_set_interrupt_flag(interrupts);
+}
