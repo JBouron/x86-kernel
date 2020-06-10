fbe922744083250b0c6ea8b7067719adf40208ff
Justinien Bouron <justinien.bouron@gmail.com>
2020-06-10 16:33:53 -0700

    FileSystem: First implementation of the filesystem interface.

---
+++ src/fs.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <types.h>
+
+// This file declares the filesystem interface. Any supported filesystem must
+// define specific functions and structs described below:
+
+// Forward declaration see definition below.
+struct file;
+
+// This structure contains pointers to functions operating on files. Those
+// functions are defined by the filesystem implementation. There is one struct
+// file_ops per filesystem and this struct will be passed to any opened file on
+// a filesystem.
+struct file_ops {
+    // Read from the file.
+    // @param file: The file to read from.
+    // @param offset: The offset where to read from within the file.
+    // @param buf: The buffer to read into.
+    // @param len: The length of the buffer in bytes.
+    // @return: The number of bytes read.
+    size_t (*read)(struct file * file, off_t offset, uint8_t * buf, size_t len);
+
+    // Write to a file.
+    // @param file: The file to write into.
+    // @param offset: The offset where to write from within the file.
+    // @param buf: The buffer containing data to be written.
+    // @param len: The length of the buffer in bytes.
+    // @return: The number of bytes written.
+    size_t (*write)(struct file * file,
+                    off_t offset,
+                    uint8_t const * buf,
+                    size_t len);
+};
+
+// Each file opened on the system has a corresponding struct file associated to
+// it that contains the state of this file. Note that if a given file on a disk
+// is opened by multiple processes then there are multiple struct file for the
+// same physical file.
+struct file {
+    // The full path of this file.
+    char const * path;
+    // The disk from which this file has been opened.
+    struct disk * disk;
+    // The file operations as defined by the filesystem from which this file was
+    // opened.
+    struct file_ops const * ops;
+    // Available data to the filesytem implementation.
+    void *fs_private;
+};
+
+// Each supported filesystem must define a struct fs which defines basic
+// operations on the filesystem. Note: If a given filesystem is used twice (i.e.
+// on two different disks), only one such struct is defined and used.
+struct fs_ops {
+    // Try to detect the file system on a disk. This function is used when the
+    // file system on a disk is unknown.
+    // @param disk: The disk.
+    // @return: true if the disk uses this filesystem, false otherwise. 
+    bool (*detect_fs)(struct disk * disk);
+
+    // Create a new file on the filesystem.
+    // @param disk: The disk on which the new file should be created.
+    // @param path: The full path of the file to be created.
+    // @return: A struct file for the new created file.
+    struct file *(*create_file)(struct disk * disk, char const * path);
+
+    // Open a file on the filesystem.
+    // @param disk: The disk to open the file from.
+    // @param path: The full path of the file to be opened.
+    // @return: A struct file for the opened file.
+    struct file *(*open_file)(struct disk * disk, char const * path);
+
+    // Delete a file from the filesystem.
+    // @param disk: The disk to delete the file from.
+    // @param path: The full path of the file to be deleted.
+    void (*delete_file)(struct disk * disk, char const * path);
+};
+
+// This struct describes a supported filesystem in this kernel. There is one
+// such struct per supported filesystem no matter how many mounted disks are
+// using this filesystem.
+struct fs {
+    // The name of the supported file system.
+    char const * name;
+    // The operations available on the filesystem.
+    struct fs_ops const * ops;
+};
