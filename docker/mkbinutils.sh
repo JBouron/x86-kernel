d264c9bd7cade3aedbaaba9ae336284e304815b4
Justinien Bouron <justinien.bouron@gmail.com>
2023-12-09 20:55:43 -0800

    Improve makefile. Removed hardcoded values and now automatically create the docker image if it does not already exist.

--- docker/mkbinutils.sh
+++ docker/mkbinutils.sh
@@ -3,8 +3,10 @@ export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
 
+NCPUS=$(lscpu | grep '^CPU(s):' | awk '{print $2}')
+
 mkdir build-binutils
 cd build-binutils
 ../binutils-2.32/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-make -j 8
+make -j $NCPUS
 make install
--- docker/mkgcc.sh
+++ docker/mkgcc.sh
@@ -4,11 +4,13 @@ export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
 # The $PREFIX/bin dir _must_ be in the PATH. We did that above.
 which -- $TARGET-as || echo $TARGET-as is not in the PATH
+
+NCPUS=$(lscpu | grep '^CPU(s):' | awk '{print $2}')
  
 mkdir build-gcc
 cd build-gcc
 ../gcc-9.1.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
-make all-gcc -j8
-make all-target-libgcc -j8
-make install-gcc -j8
-make install-target-libgcc -j8
+make all-gcc -j$NCPUS
+make all-target-libgcc -j$NCPUS
+make install-gcc -j$NCPUS
+make install-target-libgcc -j$NCPUS
